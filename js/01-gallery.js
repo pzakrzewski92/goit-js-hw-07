8f5c277e9afa850c640bd5457365c66ccb90eab9
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-28 21:56:20 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+.map(
+    (image) =>
+        `<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+<img
+  class="gallery__image"
+  src="${image.preview}"
+  data-source="${image.original}"
+  alt="${image.description}"
+/>
+</a>
+</div>`
+)
+.join("");
+
+gallery.insertAdjacentHTML("beforeend", newImages);
+
+gallery.addEventListener("click", onOpenModal);
+
+const url = instance.element().querySelector("img");
+
+function onOpenModal(event) {
+event.preventDefault();
+if (event.target.nodeName !== "IMG") {
+    return;
+}
+url.src = event.target.dataset.source;
+instance.show();
+window.addEventListener("keydown", onEscapeClick);
+}
+
+function onEscapeClick(event) {
+if (event.key === "Escape") {
+    console.log("Close modal");
+    instance.close();
+    window.removeEventListener("keydown", onEscapeClick);
+    return;
+}
+}
+console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector("div.gallery");
+
+const newImages = galleryItems
+    .map(
+        (image) =>
+            `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+    )
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", newImages);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+});
+
+console.log(galleryItems)
